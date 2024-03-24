cdb3ca84fabef9720aa20aa6155dbed14db0a962
Hamraz Hakeem <hamrazhakeem100@gmail.com>
2024-03-24 17:12:25 +0530

    Add Static Root

--- bestviewproj/settings.py
+++ bestviewproj/settings.py
@@ -17,6 +17,7 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
