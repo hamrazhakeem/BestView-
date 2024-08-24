4115683e88e371065e31fd2c40bd246cfba53d2d
Hamraz Hakeem <hamrazhakeem100@gmail.com>
2024-08-24 22:41:10 +0530

    Modify settings.py

--- bestviewproj/settings.py
+++ bestviewproj/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "16.171.25.241", "0.0.0.0", 'localhost']
+ALLOWED_HOSTS = ["127.0.0.1", "16.171.25.241", "0.0.0.0", 'localhost', '51.20.65.189']
 
 SITE_ID = 7
 
